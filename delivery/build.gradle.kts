b525f0e951f70a29adda9658c704aa1142024a92
InHo <qheogus55@naver.com>
2026-08-28 23:36:33 +0900

    chore: Flyway 마이그레이션 설정

--- delivery/build.gradle.kts
+++ delivery/build.gradle.kts
@@ -26,6 +26,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("tools.jackson.module:jackson-module-kotlin")
+	implementation("org.flywaydb:flyway-core")
+	implementation("org.flywaydb:flyway-mysql")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
